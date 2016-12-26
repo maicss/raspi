34e86ae08f3132c998999c9e458e9cf9f6d4e396
maicss <maicss_mk@163.com>
2016-12-26 21:44:51 +0800

    lalala

--- le-serails.py
+++ le-serails.py
@@ -1,43 +1,65 @@
-import sys
-from you_get import common
-import requests
-import time
-import random
-import pickle
+# coding: utf-8
 import os
+import pickle
+import random
+import time
 
+import requests
 from bs4 import BeautifulSoup as bs
+
+from you_get import common
+
+
+def down(urls):
+    for i in urls:
+        if not i.get('cached'):
+            common.output_filename = i["output_filename"]
+            try:
+                t0 = time.time()
+                common.any_download(i["url"], stream_id='1080p')
+                print("total: " + str(len(urls)))
+                print(" 耗时：" + str(int(time.time() - t0)))
+            except Exception:
+                print(i["output_filename"] + ' 下载失败')
+                with open('zhenhuan-url-list.txt', 'rb') as f:
+                    f.write(pickle.dumps(urls))
+            i['cached'] = True
+            time.sleep(2)
 
 
 if os.path.isfile('zhenhuan-url-list.txt'):
-    with open('zhenhuan-url-list.txt', 'r') as f:
+    with open('zhenhuan-url-list.txt', 'rb') as f:
         url_list = pickle.loads(f.read())
-        for i in url_list:
-            common.output_filename = i["output_filename"]
-            common.any_download(i["url"])
-            time.sleep(2)
+        down(url_list)
+
 else:
 
-    if len(sys.argv) > 2:
-        print('只需要一个url参数')
-    try:
-        assert isinstance(sys.argv[1], str)
-    except (IndexError, AssertionError):
-        print('请给出第一集的页面地址后重试')
-        # print('请给出电视剧的原始提供商的展示页面后重试')
-    
-    serial_home_url = sys.argv[1]
-    
-    
+    # if len(sys.argv) > 2:
+    #     print('只需要一个url参数')
+    # try:
+    #     assert isinstance(sys.argv[1], str)
+    # except (IndexError, AssertionError):
+    #     print('请给出第一集的页面地址后重试')
+    #     # print('请给出电视剧的原始提供商的展示页面后重试')
+
+    serial_home_url = 'http://www.le.com/ptv/vplay/2167544.html'
+
+
     def get_all_video_urls():
         first = requests.get(serial_home_url)
-        
+
         soup = bs(first.text, 'lxml')
-        
-        script_str = soup.find('script', attrs={"type": "text/javascript"}).text
+
+        script_str = soup.find(
+            'script', attrs={"type": "text/javascript"}).text
+
         params = {
-            'cid': int(script_str[script_str.find('cid:') + len('cid:'): script_str.find('pid')].strip()[:-1]),  # 频道ID
-            'id': int(script_str[script_str.find('pid:') + len('pid:'): script_str.find('vid')].strip()[:-1]),  # 专辑ID
+            # 频道ID
+            'cid': int(script_str[script_str.find('cid:') + len('cid:'): script_str.find('pid')].strip()[:-1]),
+            # 专辑ID
+
+            'id': int(script_str[script_str.find('pid:') + len('pid:'): script_str.find('vid')].strip()[:-1]),
+
             'vid': int(script_str[script_str.find('vid:') + len('vid:'): script_str.find('search_word')].strip()[:-1]),
             'platform': 'pc',
             'vip': 0,
@@ -45,19 +67,27 @@ else:
             '_': int(time.time()),
             'callback': 'jQuery' + str(random.random()) + '_' + str(time.time())
         }
-        
+
         # 获取所有剧集的ID
-        
+
         video_id_list_url = 'http://d.api.m.le.com/apipccard/dynamic'
-        
+
         video_id_list_r = requests.get(video_id_list_url, params=params)
-        
+
         video_id_list = video_id_list_r.json()
-        
+
         return video_id_list['data']['episode']['videolist']
-    
-    with open('zhenhuan-url-list.txt', 'w') as f:
-        f.write(pickle.dumps([{"output_filename": i["title"] + ' - ' + i["subTitle"] + '.mp4', "url": i["url"]}
-                              for i in get_all_video_urls()]))
 
 
+    urls = get_all_video_urls()
+
+    pick_urls = [{
+                     "output_filename": i["title"] + ' - ' + i["subTitle"] + '.mp4',
+                     "url": i["url"]
+
+                 } for i in urls]
+
+    with open('zhenhuan-url-list.txt', 'wb') as f:
+        f.write(pickle.dumps(pick_urls))
+
+    down(pick_urls)
